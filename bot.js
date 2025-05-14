cba92e1bb10e76cbc89b0c56c1c9d6ecf689f711
Sonic77734442 <102771205+Sonic77734442@users.noreply.github.com>
2025-05-14 12:46:57 +0500

    fix: use inline_keyboard for Telegram WebApp

--- bot.js
+++ bot.js
@@ -2,27 +2,26 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const app = express();
 
-// Telegram bot
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 const webAppUrl = 'https://freedom-test.vercel.app';
 
 bot.onText(/\/start/, (msg) => {
   console.log("📩 /start от", msg.chat.username);
-bot.sendMessage(msg.chat.id, "Нажми кнопку ниже, чтобы пройти тест:", {
-  reply_markup: {
-    inline_keyboard: [[{
-      text: "🧪 Пройти тест",
-      web_app: { url: webAppUrl }
-    }]]
-  }
+
+  bot.sendMessage(msg.chat.id, "Нажми кнопку ниже, чтобы пройти тест:", {
+    reply_markup: {
+      inline_keyboard: [[{
+        text: "🧪 Пройти тест",
+        web_app: { url: webAppUrl }
+      }]]
+    }
+  });
 });
 
-// 🧠 Эмуляция веб-сервера для Render
 app.get('/', (req, res) => {
   res.send('Бот работает');
 });
 
-// 🔥 Render требует открыт порт
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🌐 WebService для Render запущен на порту ${PORT}`);
